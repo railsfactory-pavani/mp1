118ca0874700cbb632340dad527b1685f419ada7
pavani@railsfactory.org <pavani@client125.(none)>
2014-07-02 10:42:08 +0530

    program to check whether a character is a vowel or a consonant

---
+++ vowel/vowel.rb
@@ -0,0 +1,9 @@
+puts "enter a character"
+ch=gets.chomp
+if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch =='o' || ch=='O' || ch == 'u' || ch == 'U')
+    puts "entered character is a vowel."
+  else
+    puts "entered character is a consonant."
+end
+ 
+
