39526d882496aaefabb46a841e44981fc2972c98
pavani@railsfactory.org <pavani@client125.(none)>
2014-07-02 14:02:53 +0530

    program to add n numbers

---
+++ addnno/addnno.rb
@@ -0,0 +1,10 @@
+puts "Enter the number of integers you want to add"
+n = gets.chomp.to_i
+sum=0
+puts "enter #{n} integers"
+ for c in 1..n
+  value=gets.chomp.to_i
+  sum = sum + value
+ end
+ puts "Sum of entered integers = #{sum}"
+  
