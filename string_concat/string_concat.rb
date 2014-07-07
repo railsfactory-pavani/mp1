29e9bfc7593b0892876bc064992157023a83f88a
Pavani <pavani@railsfactory.org>
2014-07-07 18:06:48 +0530

    program to concat two strings

--- string_concat/string_concat.rb
+++ string_concat/string_concat.rb
@@ -1,9 +1,9 @@
 a = Array.new(100)
 b = Array.new(100)
-puts "enter the first string"
-a = gets.chomp
-puts "enter the second string"
-b = gets.chomp
-a.concat(b)
+ puts "enter the first string"
+ a = gets.chomp
+ puts "enter the second string"
+ b = gets.chomp
+  a.concat(b)
 puts "String obtained on concatenation is #{a}"
 
