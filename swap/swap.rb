8d51144def7f3ee500e132ad95339f9673354294
Pavani <pavani@railsfactory.org>
2014-07-07 18:11:58 +0530

    prgram to swap the numbers

--- swap/swap.rb
+++ swap/swap.rb
@@ -2,8 +2,8 @@ puts "enter the value of x and y"
 x=gets.chomp.to_i
 y=gets.chomp.to_i
 puts "Before Swapping x = #{x} and y = #{y}"
-temp = x
-x = y
-y = temp
+  temp = x
+  x = y
+  y = temp
 puts "After Swapping x = #{x} and y = #{y}"
 
