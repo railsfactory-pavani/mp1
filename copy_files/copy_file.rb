033a076352f2af86ead273f468735673323c31d2
Pavani <pavani@railsfactory.org>
2014-07-07 17:51:50 +0530

    program to copy the contents of one file to another

--- copy_files/copy_file.rb
+++ copy_files/copy_file.rb
@@ -1,12 +1,11 @@
 puts "enter the name of the file to copy"
 source_file = gets.chomp
-source = File.open(source_file,'r')
-indata = source.read()
-puts "The input file is #{indata.length} bytes long"
+  source = File.open(source_file,'r')
+  indata = source.read()
 puts "enter the name of the target file"
 target_file = gets.chomp
-target = File.open(target_file,'w')
-target.write(indata)
+  target = File.open(target_file,'w')
+  target.write(indata)
 puts "contents are copied"
 source.close()
 target.close()
