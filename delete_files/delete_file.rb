7e6e83d93babf4508e2769946fede4069f8d5d8f
Pavani <pavani@railsfactory.org>
2014-07-07 17:56:32 +0530

    program to delete the files

--- delete_files/delete_file.rb
+++ delete_files/delete_file.rb
@@ -1,7 +1,7 @@
 puts "Enter the name of file you wish to delete"
 file_name = gets.chomp
  
-status = File.delete(file_name)
+  status = File.delete(file_name)
 
      puts "#{file_name} file deleted successfully."
  
