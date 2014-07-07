48cba4c1ddadda83731ab3f005a2416e913d0f19
Pavani <pavani@railsfactory.org>
2014-07-07 18:23:00 +0530

    program to check whether the number id palindrome or not

--- Palindrome/palindrome.rb
+++ Palindrome/palindrome.rb
@@ -1,7 +1,7 @@
 class Palindrome
   def palindrome
     puts "enter the number to check if it is a palindrome or not"
-    n=gets.chomp.to_i
+    n = gets.chomp.to_i
     reverse = 0
     temp = n
       while (temp != 0) do
@@ -9,13 +9,13 @@ class Palindrome
         reverse = reverse + temp%10
         temp = temp/10
       end 
-   if ( n == reverse )
+   if n == reverse 
       puts "#{n} is a palindrome number"
    else
       puts "#{n} is not a palindrome number"
    end
   end
 end
-p=Palindrome.new
+p = Palindrome.new
 p.palindrome
     
