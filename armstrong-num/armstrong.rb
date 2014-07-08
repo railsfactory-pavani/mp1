8144c2a1d4fa2a0c324e0335b7cc69e1b8198c1f
Pavani <pavani@railsfactory.org>
2014-07-08 09:46:45 +0530

    program to check whether the number is armstrong number or not

--- armstrong-num/armstrong.rb
+++ armstrong-num/armstrong.rb
@@ -1,13 +1,13 @@
 sum = 0
 puts "enter a integer"
-n=gets.to_i
+n = gets.to_i
 temp = n
   while temp != 0 do
-    remainder = temp%10;
-    sum = sum + remainder*remainder*remainder;
-    temp = temp/10;
+    remainder = temp % 10;
+    sum = sum + remainder * remainder * remainder;
+    temp = temp / 10;
   end
-   if ( n == sum )
+   if n == sum 
       puts "Entered number is an armstrong number"
    else
       puts "Entered number is not an armstrong number"
