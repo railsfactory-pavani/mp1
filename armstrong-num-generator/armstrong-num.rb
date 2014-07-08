b757ecb5b56ac3a3a2eb8a941941bf254eb37d1a
Pavani <pavani@railsfactory.org>
2014-07-08 09:44:29 +0530

    program to generate armstrong numbers

--- armstrong-num-generator/armstrong-num.rb
+++ armstrong-num-generator/armstrong-num.rb
@@ -1,16 +1,16 @@
 num = 0
 sum = 0
 puts "Enter an integer upto which you want to find armstrong numbers"
-num=gets.to_i
+num = gets.to_i
 puts "Following armstrong numbers are found from 1 to #{num}"
-for c in 1..num
- temp = c
-  while temp != 0 do 
-       r = temp%10
+  for c in 1..num
+   temp = c
+   while temp != 0 do 
+       r = temp % 10
        sum = sum + r*r*r
-       temp = temp/10
-  end
-    if ( c == sum )
+       temp = temp / 10
+   end
+     if c == sum 
        puts " #{c}"
      end
     sum = 0
