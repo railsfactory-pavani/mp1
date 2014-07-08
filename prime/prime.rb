a044c747a27907c7013d7eafa9fa6307ae99313a
Pavani <pavani@railsfactory.org>
2014-07-08 10:27:01 +0530

    program to check whether the number is prime or not

--- prime/prime.rb
+++ prime/prime.rb
@@ -1,17 +1,17 @@
 puts "enter a number"
-a=gets.chomp
-n=Integer(a)
-x=true
+a = gets.chomp
+n = Integer(a)
+x = true
 if n == 2
    puts "Prime number."
  else
    for c in 2..(n-1)
     if n % c == 0
-     x=false 
+     x = false 
     end
    end
  end
-   if x==true
+   if x == true
       puts "prime number."
    else
       puts "Not a Prime number."
