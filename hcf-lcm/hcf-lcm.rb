940b516495778b293e5da607d9a8cfa9504681c0
Pavani <pavani@railsfactory.org>
2014-07-08 10:05:10 +0530

    program to computs hcf and lcm

--- hcf-lcm/hcf-lcm.rb
+++ hcf-lcm/hcf-lcm.rb
@@ -1,16 +1,16 @@
 puts "enter two integers"
-x=gets.chomp.to_i
-y=gets.chomp.to_i
+x = gets.chomp.to_i
+y = gets.chomp.to_i
 a = x
 b = y
  while (b != 0) do
-    t = b;
-    b = a % b;
-    a = t;
+    t = b
+    b = a % b
+    a = t
  end
  
-  gcd = a;
-  lcm = (x*y)/gcd;
+  gcd = a
+  lcm = (x * y)/gcd
  
   puts "Greatest common divisor of #{x} and #{y} is #{gcd}"
   puts "Least common multiple of #{x} and #{y} is #{lcm}"
